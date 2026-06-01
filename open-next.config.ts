1295937aec75ad127131aff7d867263e3ef34642
Muguett-DBY <85372096+Muguett-DBY@users.noreply.github.com>
2026-06-01 20:58:14 +1000

    Upgrade RocoDex Vercel runtime

--- open-next.config.ts
+++
@@ -1,9 +0,0 @@
-import {
-  defineCloudflareConfig,
-  type OpenNextConfig,
-} from "@opennextjs/cloudflare";
-
-export default {
-  ...defineCloudflareConfig(),
-  buildCommand: "npm run next:build",
-} satisfies OpenNextConfig;
